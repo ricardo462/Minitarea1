2808471d10528372d3692e3be6ac617fc67f7323
Ricardo <ricardo.rg462@gmail.com>
2021-05-07 18:11:08 -0400

    hice un cambio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Estoy en la rama develop
+        // haces un camcio
         // Creating Pokemon's
         WaterPokemon squirtle = new WaterPokemon("Ed", "Squirtle", 100);
         FirePokemon charmander = new FirePokemon("Edd", "Charmander", 100);
